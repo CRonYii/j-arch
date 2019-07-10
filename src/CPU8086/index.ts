badd79c0c8f16310d00cb8432ead13733308f822
Rongyi Chen <kenchenrong@gmail.com>
2019-07-10 19:00:25 +0800

    added TODO comments

--- src/CPU8086/index.ts
+++ src/CPU8086/index.ts
@@ -7,25 +7,40 @@ import { SegmentRegister } from "./registers/SegmentRegister";
 export class CPU8086 {
 
     // General Purpose Registers
-    private readonly ax = new GeneralPurposeRegister();
-    private readonly bx = new GeneralPurposeRegister();
-    private readonly cx = new GeneralPurposeRegister();
-    private readonly dx = new GeneralPurposeRegister();
+    private readonly ax = new GeneralPurposeRegister(); // Accumulator: Multiply, divide, I/O
+    private readonly bx = new GeneralPurposeRegister(); // Base: Count for loops, shifts
+    private readonly cx = new GeneralPurposeRegister(); // Count: Pointer to base addresss (data)
+    private readonly dx = new GeneralPurposeRegister(); // Data: Multiply, divide, I/O
 
     // Pointer and Index Registers
-    private readonly sp = new PointerRegister();
-    private readonly bp = new PointerRegister();
-    private readonly si = new PointerRegister();
-    private readonly di = new PointerRegister();
+    private readonly sp = new PointerRegister(); // Pointer to top of stack
+    private readonly bp = new PointerRegister(); // Pointer to base address (stack)
+    private readonly si = new PointerRegister(); // Source string/index pointer
+    private readonly di = new PointerRegister(); // Destination string/index pointer
 
     // Segment Registers
-    private readonly cs = new SegmentRegister('0xFFFF');
-    private readonly ds = new SegmentRegister();
-    private readonly ss = new SegmentRegister();
-    private readonly es = new SegmentRegister();
+    private readonly cs = new SegmentRegister('0xFFFF'); // Code Segment (pg.46)
+    private readonly ds = new SegmentRegister(); // Data Segment
+    private readonly ss = new SegmentRegister(); // Stack Segment
+    private readonly es = new SegmentRegister(); // Extra Segment
 
     // Other Registers
     private readonly ip = new InstructionPointerRegister();
     private readonly flags = new FlagsRegister();
+
+    /**
+     * TODO: implement the following instructions:
+     * mov: (Register, BitBufferable)
+     * add: (Register, BitBufferable)
+     * jmp: (BitBufferable, BitBufferable) | (Register) (pg.45)
+     */
+
+     /**
+      * TODO: implement the mechanism to execute instructions with CS and IP (pg.46)
+      * 1. Read the addresses from CS and IP, compute the actual memory address using the address adder
+      * 2. Retrieve the data stored in the memory address and the length of the insturcion
+      * 3. Write the data into Instruction Buffer, increment the IP
+      * 4. Execute the instruction, repeat from step 1
+      */
 
 }
--- src/CPU8086/registers/GeneralPurposeRegister.ts
+++ src/CPU8086/registers/GeneralPurposeRegister.ts
@@ -1,6 +1,7 @@
 import { Register16 } from "./Register16";
 import { BitBufferable } from "../arch/BitBuffer";
 
+// TODO: This type of register can be treated as two 8-bit registers
 export class GeneralPurposeRegister extends Register16 {
 
     constructor(initialValue?: BitBufferable) {
